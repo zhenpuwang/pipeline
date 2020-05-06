62d74e656d99b334aabe61fd3183b6613705844a
王振普 <30329981+zhenpuwang@users.noreply.github.com>
2020-05-06 10:00:37 +0800

    Update start_tomcat.sh

--- start_tomcat.sh
+++ start_tomcat.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 TOMCAT_DIR=/root/soft/apache-tomcat-8.5.47/bin
+export PATH=/root/soft/jdk1.8.0_172/bin:$PATH
 count=' ps -ef | grep -w tomcat | grep -v "grep" | wc -1 '
 echo $count
 if [$count -le 0]; then
-        echo "tomcat is not run"
-        cd $TOMCAT_DIR
-        sudo ./startup.sh
-        echo "Tomcat 启动成功"
+        echo "tomcat is not run" && cd $TOMCAT_DIR        
+        echo "移动项目"
+        sudo ./startup.sh && echo "Tomcat 启动成功"   
 else
-        echo "tomcat is run"
-        cd $TOMCAT_DIR
+        echo "tomcat is run" && cd $TOMCAT_DIR    
         sudo ./shutdown.sh
-        sudo ./startup.sh
-        echo "Tomcat 启动成功"
+        echo "移动项目"
+        sudo ./startup.sh && echo "Tomcat 启动成功"       
 fi
