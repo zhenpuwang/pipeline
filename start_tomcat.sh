ed03c4b380bcca4035798fbd904c3a1d2aa30196
王振普 <30329981+zhenpuwang@users.noreply.github.com>
2020-05-06 10:06:22 +0800

    Update start_tomcat.sh

--- start_tomcat.sh
+++ start_tomcat.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 TOMCAT_DIR=/root/soft/apache-tomcat-8.5.47/bin
+export JAVA_HOME=/root/soft/jdk1.8.0_172
+export JRE_HOME=/root/soft/jdk1.8.0_172/jre
+export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
+export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$JAVA_HOME:$PATH
 count=`ps -ef | grep -w tomcat | grep -v "grep" | wc -1`
 echo $count
 if [$count -le 0]; then
